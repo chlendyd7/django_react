e7e9427e20b2e903ede5d6fedbc5089897c08418
Choi Du Young <chlendyd7@naver.com>
2023-10-13 12:42:13 +0900

    pip freeze

--- config/config/urls.py
+++ config/config/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView, RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url = '/instagram/'), name='root'),
     path('admin/', admin.site.urls),
     path('instagram/', include('instagram.urls')),
 ]
--- config/instagram/views.py
+++ config/instagram/views.py
@@ -1,13 +1,24 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Post
 
 # Create your views here.
-def post_detail(request: HttpRequest, pk : int) -> HttpResponse:
-    response = HttpResponse()
-    response.write("Hello World")
-    response.write("Hello World")
-    response.write("Hello World")
-    return response
+# def post_detail(request: HttpRequest, pk : int) -> HttpResponse:
+#     response = HttpResponse()
+#     response.write("Hello World")
+#     response.write("Hello World")
+#     response.write("Hello World")
+#     return response
+
+# post_detail = DetailView.as_view(model=Post, pk_url_kwarg='id')
+
 
 def archives_year(request, year):
     return HttpResponse(f"{year}년 archives")
+
+def post_detail(request: HttpRequest, pk:int) -> HttpResponse:
+    post = Post.objects.get(pk=pk)
+    return render(request, 'instagram/post_detail.html', {
+        'post' : post
+    })
